420928cef340710470461131f5deed756756705c
Dice <tetradice@gmail.com>
2019-09-01 23:48:21 +0900

    Improve state declaration

--- index.d.ts
+++ index.d.ts
@@ -2,17 +2,17 @@ import { Middleware, VNode, Action, Dispatchable, Children } from "hyperapp";
 
 declare const componentHandler: Middleware;
 
-interface RequiredProps {
+interface RequiredProps<MainState> {
     key?: string | number;
-    state: object;
+    state: MainState;
 }
 
 type ComponentContext = { name: string, key: string | number };
 
-interface ComponentParam<Props, PState> {
+interface ComponentParam<Props, PState, MainState> {
     name?: string;
     init?: () => PState;
-    view: (c: ComponentContext, partialState: PState, props: Props & RequiredProps, children: Children[]) => VNode | null;
+    view: (c: ComponentContext, partialState: PState, props: Props & RequiredProps<MainState>, children: Children[]) => VNode | null;
 }
 
-export function component<Props, PState>(params: ComponentParam<Props, PState>): (props: Props & RequiredProps, children: Children[]) => VNode | null;
+export function component<Props, PState, MainState>(params: ComponentParam<Props, PState, MainState>): (props: Props & RequiredProps<MainState>, children: Children[]) => VNode | null;
