b86c52338fdcd899b2ef403f997388ee0ff54352
Dice <tetradice@gmail.com>
2019-09-01 23:50:36 +0900

    Add polyfill Object.assign

--- src/index.js
+++ src/index.js
@@ -23,6 +23,29 @@ function getPartialState(init, state, path) {
     }
 }
 
+// from MDN <https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign>
+function objectAssign(target, varArgs) { // .length of function is 2
+    if (target == null) { // TypeError if undefined or null
+        throw new TypeError('Cannot convert undefined or null to object');
+    }
+
+    var to = Object(target);
+
+    for (var index = 1; index < arguments.length; index++) {
+        var nextSource = arguments[index];
+
+        if (nextSource != null) { // Skip over if undefined or null
+            for (var nextKey in nextSource) {
+                // Avoid bugs when hasOwnProperty is shadowed
+                if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+                    to[nextKey] = nextSource[nextKey];
+                }
+            }
+        }
+    }
+    return to;
+}
+
 export function componentHandler(baseDispatch) {
     var currentState = undefined;
 
@@ -59,7 +82,7 @@ export function componentHandler(baseDispatch) {
                     // new state
                     var newPartialState = target[1];
 
-                    var newState = Object.assign({}, currentState);
+                    var newState = objectAssign({}, currentState);
                     if (newState["__components__"] === undefined) {
                         newState["__components__"] = {};
                     }
